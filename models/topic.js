520fbf3e5f5a9ed7cde5233bfcfd205e9e02d59d
xiongfeng <xiongfeng@qiaobutang.com>
2014-11-20 17:57:48 +0800

    blank

--- models/topic.js
+++ models/topic.js
@@ -26,16 +26,4 @@ TopicSchema.index({top: -1, last_reply_at: -1});
 TopicSchema.index({last_reply_at: -1});
 TopicSchema.index({author_id: 1, create_at: -1});
 
-TopicSchema.virtual('tabName').get(function () {
-  var tab = this.tab;
-  var pair = _.find(config.tabs, function (_pair) {
-    return _pair[0] === tab;
-  });
-  if (pair) {
-    return pair[1];
-  } else {
-    return '';
-  }
-});
-
 mongoose.model('Topic', TopicSchema);
